bd41280558d393db821ba057247e887341e8eecd
Lukas Klappert <lklappert.mmt-m2016@fh-salzburg.ac.at>
2016-12-21 17:40:41 +0100

    cardsAndGoals call

--- apis/soccer/api.js
+++ apis/soccer/api.js
@@ -13,10 +13,21 @@ module.exports = function(app) {
   connection.connect();
 
   app.get('/soccer', function(req, res) {
-    debugger
-    res.send({
+    res.json({
       code: 418,
       message: 'I\'m a teapot'
     })
   });
+
+  app.get('/soccer/cardsAndGoals', function(req, res) {
+    connection.query('SELECT goal_min, COUNT(goal_min) as count FROM sz_fussball_matches_goal WHERE goal_min <= 90 GROUP BY goal_min ORDER BY goal_min ASC', function(err, goals, field) {
+      connection.query('SELECT card_min, COUNT(card_min) as count FROM sz_fussball_matches_card WHERE card_min <= 90 GROUP BY card_min ORDER BY card_min ASC', function(err, cards, field) {
+        res.json({
+          goals: goals,
+          cards: cards
+        });
+      });
+    });
+  });
+
 }
