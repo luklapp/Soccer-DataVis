27050eb35f8a9311ef70e58485983e9675d2fe90
Fabian Hoffmann <fhoffmann.mmt-m2016@fh-salzburg.ac.at>
2017-01-12 13:27:12 +0100

    Filter card and goal calls by min and max Minute

--- apis/soccer/api.js
+++ apis/soccer/api.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = function(app) {
+  var mysql      = require('mysql');
   var connection = require('./db.js');
   connection.connect();
 
@@ -23,20 +24,28 @@ module.exports = function(app) {
   });
 
   app.get('/soccer/goals', function(req, res) {
-    let minuteMin = req.query.minuteMin || 0
-    let minuteMax = req.query.minuteMax || 90
+    let minuteMin = parseInt(req.query.minuteMin) || 0
+    let minuteMax = parseInt(req.query.minuteMax) || 90
 
-    connection.query('SELECT goal.goal_min, club.club_name, COUNT(goal.goal_min) as count FROM sz_fussball_matches_goal goal LEFT JOIN sz_fussball_club club ON goal.goal_clubID = club.club_id GROUP BY goal.goal_min, goal.goal_clubID', function(err, goals, field) {
+    var sql = 'SELECT goal.goal_min, club.club_name, COUNT(goal.goal_min) as count FROM sz_fussball_matches_goal goal LEFT JOIN sz_fussball_club club ON goal.goal_clubID = club.club_id WHERE goal.goal_min >= ? AND goal.goal_min <= ? GROUP BY goal.goal_min, goal.goal_clubID';
+    var inserts = [minuteMin, minuteMax];
+    sql = mysql.format(sql, inserts);
+
+    connection.query(sql, function(err, goals, field) {
       res.json({goals})
     });
 
   });
 
   app.get('/soccer/cards', function(req, res) {
-    let minuteMin = req.query.minuteMin || 0
-    let minuteMax = req.query.minuteMax || 90
+    let minuteMin = parseInt(req.query.minuteMin) || 0
+    let minuteMax = parseInt(req.query.minuteMax) || 90
 
-    connection.query('SELECT card.card_min, club.club_name, COUNT(card.card_min) as count FROM sz_fussball_matches_card card LEFT JOIN sz_fussball_club club ON card.card_clubID = club.club_id GROUP BY card.card_min, card.card_clubID', function(err, cards, field) {
+    var sql = 'SELECT card.card_min, club.club_name, COUNT(card.card_min) as count FROM sz_fussball_matches_card card LEFT JOIN sz_fussball_club club ON card.card_clubID = club.club_id WHERE card.card_min >= ? AND card.card_min <= ? GROUP BY card.card_min, card.card_clubID';
+    var inserts = [minuteMin, minuteMax];
+    sql = mysql.format(sql, inserts);
+
+    connection.query(sql, function(err, cards, field) {
       res.json({cards})
     });
 
