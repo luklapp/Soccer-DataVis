6c54997f6b8ac01d7636a6f7ef64de17646c2df3
Lukas Klappert <lklappert.mmt-m2016@fh-salzburg.ac.at>
2017-01-12 14:30:18 +0100

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cors = require('cors');
 
 var app = express();
 var http = require('http').Server(app);
 
 app.use(logger('dev'));
+app.use(cors());
 
 // Load APIs
 const normalizedPath = require("path").join(__dirname, "./apis/");
@@ -27,7 +29,22 @@ app.get('/', function(req, res) {
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
   next(err);
+
 });
 
 // production error handler
